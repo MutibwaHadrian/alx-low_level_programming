4da7f54e51b4170f990696d735f86fa4b10b0937
Mutibwahadrian <hadrianmutibwa@gmail.com>
2023-03-20 09:34:56 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,14 +11,12 @@
 	 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	if (d)
-	{
-
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-
-	}
+  if (d == NULL)
+    ;
+  else
+    {
+      d->name = name;
+      d->age = age;
+      d->owner = owner;
+    }
 }
-
