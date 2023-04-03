a87f518a128c98b15c85aa7761531a70ba6faf8f
Mutibwa Hadrain <hadrianmutibwa@gmail.com>
2023-04-03 10:59:29 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int place = 0;
+	int count = 0;
 
 	while (h)
 	{
 
 		printf("%i\n", h->n);
 		h = h->next;
-		place++;
+		count++;
 	}
-	return (place);
+	return (count);
 }
