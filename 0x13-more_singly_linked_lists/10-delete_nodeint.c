37e41a255b2ec3cc0bf0e1232898540f1325fe16
Mutibwa Hadrain <hadrianmutibwa@gmail.com>
2023-04-03 18:01:54 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	}
 
-	for (value = 0; value < (index - 1); node++)
+	for (value = 0; value < (index - 1); value++)
 	{
 		if (fun->next == NULL)
 		{
