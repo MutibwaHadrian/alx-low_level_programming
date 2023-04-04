1dda86793cb6db08a504434acb4ac0a6f6946d6a
Mutibwa Hadrain <hadrianmutibwa@gmail.com>
2023-04-03 19:18:40 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,10 +8,6 @@
  * @head: a pointer to the head of the listint_t list
  *
  * Return: the number of nodes in the list
- * This function can print lists with a loop
- * You should go through the list only once
- * If the function fails, exit the program with status 98
- * Output format: see example
  */
 size_t print_listint_safe(const listint_t *head)
 {
