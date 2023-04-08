a40191abb3d84b815564cbb266d537cfcf77eecb
Mutibwa Hadrain <hadrianmutibwa@gmail.com>
2023-04-07 18:01:00 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
+#define BITS_PER_LONG (sizeof(unsigned long int) * 8)
+
+
 /**
  * get_bit - a function that returns the value of a bit at a given index.
- * @n: unsigned long interger
+ * @n: The unsigned long interger
  * @index: the index, starting from 0 of the bit you want to get
  *
- * Returns: the value of the bit at index index or -1 if an error occured
+ * Returns: the value of the bit at index index, or -1 if an error occured
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index >= BITS_PER_LONG)
 	{
 		return (-1);
 	}
