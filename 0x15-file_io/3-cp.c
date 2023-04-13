6c2758c8288c42ee19c9ed4be3b3bf293dd72e33
Mutibwa Hadrain <hadrianmutibwa@gmail.com>
2023-04-13 07:23:09 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <errno.h>
+#include "main.h"
 
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 
-void print_error_and_exit(int exit_code, const char *error_message, const char
-*file_name)
+void error_exit(int exit_code, const char *error_message)
 {
-	dprintf(STDERR_FILENO, error_message, file_name);
+	dprintf(STDERR_FILENO, "Error: %s\n", error_message);
 	exit(exit_code);
 }
 
 int main(int argc, char *argv[])
 {
+	int fd_from, fd_to;
+	ssize_t num_read, num_written;
+	char buf[BUF_SIZE];
+
 	if (argc != 3)
 	{
-		print_error_and_exit(97, "Usage: cp file_from file_to\n", NULL);
+		error_exit(97, "Usage: cp file_from file_to");
 	}
 
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-
-	int fd_src = open(file_from, O_RDONLY);
-
-	if (fd_src == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		print_error_and_exit(98, "Error: Can't read from file %s\n", file_from);
+		error_exit(98, strerror(errno));
 	}
 
-	int fd_dest = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (fd_dest == -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
 	{
-		print_error_and_exit(99, "Error: Can't write to %s\n", file_to);
+		error_exit(99, strerror(errno));
 	}
 
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
-
-	while ((bytes_read = read(fd_src, buffer, BUFFER_SIZE)) > 0)
+	while ((num_read = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
-		bytes_written = write(fd_dest, buffer, bytes_read);
-		if (bytes_written == -1)
+		num_written = write(fd_to, buf, num_read);
+		if (num_written != num_read)
 		{
-			print_error_and_exit(99, "Error:
-					Can't write to %s\n", file_to);
+			error_exit(99, strerror(errno));
 		}
 	}
 
-	if (bytes_read == -1)
+	if (num_read == -1)
 	{
-		print_error_and_exit(98, "Error:
-				Can't read from file %s\n", file_from);
+		error_exit(98, strerror(errno));
 	}
 
-	if (close(fd_src) == -1 || close(fd_dest) == -1)
+	if (close(fd_from) == -1)
 	{
-		print_error_and_exit(100, "Error:
-				Can't close fd %d\n", errno);
+		error_exit(100, strerror(errno));
+	}
+
+	if (close(fd_to) == -1)
+	{
+		error_exit(100, strerror(errno));
 	}
 
 	return (0);
